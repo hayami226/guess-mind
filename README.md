36fb266905bfb1e5f9fd6f326303f00288020d9c
hayami226 <59946552+hayami226@users.noreply.github.com>
2021-04-27 21:59:20 +0900

    20210427 Commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # guess-mind
+
 Realtime Drawing Game built with SocketIO, Gulp and Node
+
+
+## To Do:
+
+- [x] Server
+- [x] Pug
+- [x] Static
+- [x] ESlint
+- [ ] SocketIO
---
+++ src/server.js
@@ -0,0 +1,37 @@
+import { join } from "path";
+import express from "express";
+import socketIO from "socket.io";
+import logger from "morgan";
+
+const PORT = 4000;
+const app = express();
+app.set("view engine", "pug");
+app.set("views", join(__dirname, "views"));
+app.use(logger("dev"));
+app.use(express.static(join(__dirname, "static")));
+app.get("/", (req, res) => res.render("home"));
+
+const handleListening = () =>
+  console.log(`✅ Server running: http://localhost:${PORT}`);
+
+const server = app.listen(PORT, handleListening);
+
+const io = socketIO(server);
+
+// soket
+// first connection
+// on : listen
+// emit : send event to connection socket
+// boradcast : send event to other socket
+
+io.on("connection", (socket) => {
+  socket.on("newMessage", ({ message }) => {
+    socket.broadcast.emit("messageNotif", {
+      message,
+      nickname: socket.nickname || "Bot",
+    });
+  });
+  socket.on("setNickname", ({ nickname }) => {
+    socket.nickname = nickname;
+  });
+});
